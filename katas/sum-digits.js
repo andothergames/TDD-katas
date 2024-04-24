bad6fb9048a1138a15b9448edc08d5305149a60e
Joy <32204909+andothergames@users.noreply.github.com>
2024-04-24 08:34:54 +0100

    complete sumDigits function and tests

--- katas/sum-digits.js
+++ katas/sum-digits.js
@@ -1,4 +1,9 @@
-function sumDigits(n) {
+function sumDigits(number) {
+    if (number < 10 && number >= 0) return number;
+    const stringNumber = number.toString().replace(/[^0-9]/g, "");
+    const numberArray = stringNumber.split("").map(Number);
+    const numbersSum = numberArray.reduce((accumulator, currentValue) => (accumulator + currentValue));
+    return numbersSum;
 
 }
 
